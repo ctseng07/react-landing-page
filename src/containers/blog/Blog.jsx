06af3d7dbe0d89ec54696f7d4b1e88379cb79e1e
Caleb Tseng <calebtseng07@gmail.com>
2023-07-22 03:58:08 -0700

    Design article and blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
-import blog01 from '../../assets/blog01.png';
-import blog02 from '../../assets/blog02.png';
-import blog03 from '../../assets/blog03.png';
-import blog04 from '../../assets/blog04.png';
-import blog05 from '../../assets/blog05.png';
 import './blog.css';
 
 const Blog = () => {
     return (
-        <div className='gpt3__blog section__padding'>
-            <div className='gpt3__blog-content'>
+        <div className='gpt3__blog section__padding' id="blog">
+            <div className='gpt3__blog-heading'>
                 <h1 className='gradient__text'>A lot is happening,
                     We are blogging about it.</h1>
             </div>
-            <div className='gpt3__blog-image'>
-                <img src={blog01} alt='blog01' />
-                <img src={blog02} alt='blog02' />
-                <img src={blog03} alt='blog03' />
-                <img src={blog04} alt='blog04' />
-                <img src={blog05} alt='blog05' />
+            <div className='gpt3__blog-container'>
+                <div className='gpt3__blog-container_groupA'>
+                    <Article imgUrl={blog01} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us exlore how it is?" />
+                </div>
+                <div className='gpt3__blog-container_groupB'>
+                    <Article imgUrl={blog02} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog03} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog04} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog05} date="Sep 26, 2021" title="GPT-3 and Open AI is the future. Let us exlore how it is?" />
+                </div>
             </div>
         </div>
     )
